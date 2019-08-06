03f5ab725bc354583561d0361597cd6d351cf515
Mahmudul Hasan Sumon <mhsumonnn@gmail.com>
2019-08-06 22:53:46 +0600

    Bug Fix

--- index.php
+++ index.php
@@ -23,9 +23,8 @@
 
     <div class="container">
       <div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h2>ID Card Generator</h2>
-        <!--<p class="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p> -->
+        <h2>JKKNIU ID Card Generator</h2>
+        <p class="lead">Provide all the informaiton below and Click Generate Button</p>
       </div>
 
       <div class="row">
